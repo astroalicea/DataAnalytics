c54654762e60ef2e07be76c5378ea64eef483d69
astroalicea <chonny507@gmail.com>
2026-04-13 14:37:20 -0400

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,7 @@
 /*
 a database for information on movies, rentals and used by employees.
 */
+
+use sakila;
+select * from actor; -- Retrieved 200 records
+select * from film; -- Retrieved 1000 records
