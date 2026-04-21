84e7a9e6600f4cdbc55fbf855a967d1286188bde
astroalicea <chonny507@gmail.com>
2026-04-21 17:33:19 -0400

    Complete 1C query practice

--- week-03/1B_query_practice.sql
+++ week-03/1B_query_practice.sql
@@ -2,36 +2,36 @@ USE northwind;
 -- 1. Write a query to list the product id, product name, and unit price of every product that
 -- Northwind sells.
 
--- SELECT ProductID, ProductName, UnitPrice  FROM products
+ SELECT ProductID, ProductName, UnitPrice  FROM products
 
 -- 2. Write a query to identify the products where the unit price is $7.50 or less.
 
--- WHERE UnitPrice <= 7.50;
+ WHERE UnitPrice <= 7.50;
 
 -- 3. What are the products that we carry where we have no units on hand, but 1 or more units are on backorder? Write a query that answers this question.
--- SELECT ProductName, ProductID, UnitsInStock, UnitsOnOrder FROM products
--- WHERE UnitsInStock = 0 AND UnitsOnOrder >= 1;
+ SELECT ProductName, ProductID, UnitsInStock, UnitsOnOrder FROM products
+ WHERE UnitsInStock = 0 AND UnitsOnOrder >= 1;
 
 
 -- 4. Examine the products table. How does it identify the type (category) of each item sold? Where can you find a list of all categories? Write a set of queries to answer thesequestions, ending with a query that creates a list of all the seafood items we carry.
--- SELECT * FROM categories;
--- SELECT * FROM products
--- WHERE CategoryID = 8;
+ SELECT * FROM categories;
+ SELECT * FROM products
+ WHERE CategoryID = 8;
 
 -- 5. Examine the products table again. How do you know what supplier each product comes from? Where can you find info on suppliers? Write a set of queries to find the specific identifier for "Tokyo Traders" and then find all products from that supplier.
--- SELECT SupplierID, CompanyName
--- FROM suppliers
--- WHERE CompanyName = 'Tokyo Traders';
+ SELECT SupplierID, CompanyName
+ FROM suppliers
+ WHERE CompanyName = 'Tokyo Traders';
 
--- SELECT * FROM products
--- WHERE SupplierID = 4;
+SELECT * FROM products
+WHERE SupplierID = 4;
 
 -- 6. How many employees work at northwind? What employees have "manager"
 -- somewhere in their job title? Write queries to answer each question.
 
 -- SELECT COUNT(*) FROM employees
 
--- SELECT FirstName, LastName, Title FROM employees
--- WHERE Title LIKE "%Manager%"
+SELECT FirstName, LastName, Title FROM employees
+WHERE Title LIKE "%Manager%";
 
 
