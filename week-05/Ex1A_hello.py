9fa68bedc81d7a1670e939c313fd5537a39957f1
astroalicea <chonny507@gmail.com>
2026-05-04 14:11:11 -0400

    Add Displaying dollars and cents.

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,21 @@
 print('Hello world!')
+
+message = 'Hello world!'
+print(message)
+
+# "Hello world prints twice because vscode reads both times it is wrriten."
+
+#Displaying dollars and cents
+
+dollars = 3
+cents = .50
+print(dollars + cents)
+# It prints 3.5
+cents = cents + .25
+print(dollars + cents)
+# It prints 3.75
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str, c_str)
+# i decided to use the comma instead. less keystrokes
