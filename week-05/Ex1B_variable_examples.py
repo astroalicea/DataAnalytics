c67e309a9c4eca32f216cd387b792186b31d9ce7
astroalicea <chonny507@gmail.com>
2026-05-04 14:55:01 -0400

    Add Lab 2

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,23 @@
+customer_id = "23"
+customer_name = "Jiro"
+customer_gender = "Male"
+customer_dob = "05/27/1990"
+drivers_license_num = "AB-123456789"
+auto_policy_num = "987654321"
+# I used strings becuase the are all unique data types.
+
+name = "Luis Alicea"
+city = "Rockville"
+State = "Maryland"
+
+# a) False, def, if, raise, None, del, import, return, True, elif, in, try, and, else, is, while, as, except, lambda, with, assert, finally, nonlocal, yield, break, for, not, class, form, or, continue, global, pass
+
+# lambda - A lambda function in Python is an anonymous function. It can take any number of arguments but only have a single expression.
+
+# yield- The yield function ends a function and returns an ^iterator^.
+
+# global- Accessing a global variable is simple as any other variable but to modify a global variable, you need to use the global keyword.
+
+# import- This statement is used to import modules to the project.
+
+# return- This keyword is used to exit a function or a method and return some value.
