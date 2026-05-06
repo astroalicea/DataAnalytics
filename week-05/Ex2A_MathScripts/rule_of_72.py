679fd738bb2164716ea5cff933a244f8aef1e5ac
astroalicea <chonny507@gmail.com>
2026-05-06 14:30:48 -0400

    complete lab 4

--- week-05/Ex2A_MathScripts/rule_of_72.py
+++ week-05/Ex2A_MathScripts/rule_of_72.py
@@ -1,8 +1,17 @@
 # Rule of 72, years to double money
 
-savings = 77064.49
-interest_rate = 4.20
+savings = float(input("What is your current savings? "))
+
+interest_rate = float(input("What is your interest rate? "))
+
 savings_double = savings * 2
+
 years_to_double = 72 / interest_rate
 
 print(f"Your current savings is ${savings:,.2f}. At a {interest_rate:.2f}% interest rate, your savings account will be worth ${savings_double:,.2f} in {years_to_double:.1f} years.")
+
+# Possible pitfalls with input()
+# 1. If user inputs letters or words instead of numbers,
+#    the script will give ValueError.
+# 2. If user user enters 0 for the interest rate, the script will crash
+#    with a ZeroDivisionError since you cant devide my zero.
