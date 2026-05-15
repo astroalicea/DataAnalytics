bf7c20b4b7e48a7b4a74883bc14a5d044a0eb0cb
astroalicea <chonny507@gmail.com>
2026-05-15 15:04:10 -0400

    Week 6 Ex4A Lab 1 - read write files

---
+++ week-06/Ex4A_ReadWriteFiles/open_and_read.py
@@ -0,0 +1,19 @@
+# Open and read the full file
+f = open('about_me.txt', 'r')
+
+#Next 4 lines using readline()
+first_50 = f.read(50)
+
+#Next 4 lines using readline()
+next_four = []
+for i in range(4):
+    next_four.append(f.readline())
+
+#Remaining lines using readlines()
+last_chunk = f.readlines()
+
+f.close()
+
+print(f"First 50 characters: {first_50}")
+print(f"Next four lines, as list by line: {next_four}")
+print(f"Next 100 characters, as list by line, rounded up to complete lines {last_chunk}")
---
+++ week-06/Ex4A_ReadWriteFiles/open_and_write.py
@@ -0,0 +1,4 @@
+f = open('about_me.txt', 'a')
+f.write('\nIf I could do anything for my perfect night out: ')
+f.write('dinner at nice restaurant, followed by dessert, then go see a world class DJ.')
+f.close()
